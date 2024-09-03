1454a8f2a882accd079cfc189a308026d0d2b533
Steven-mejia726 <st559049@ucf.edu>
2024-09-03 19:53:06 +0000

    Creating skeleton code

--- fracture.c
+++ fracture.c
@@ -1,7 +1,41 @@
+// Inputting libraries 
 #include <stdio.h>
 # include <math.h>
 
-void function1 () 
+//Creating main function
+int main(int argc, char **argv) 
 {
   
+
+  return 0;
+}
+
+//Creating function to calculate distance between two points
+double calculateDistance()
+{
+
+}
+
+//Creating function to calculate perimeter of a city 
+double calculatePerimeter() 
+{
+
+}
+
+//Creating function that calculates the area of a city encompessed by two points
+double calculateArea()
+{
+
+}
+
+//Creating a function that calculates the width of the city
+double calculateWidth()
+{
+
+}
+
+//Creating a function that calculates the height of the city
+double caluclateHeight()
+{
+
 }
