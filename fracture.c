3d39993665c3b00a9dc738ef8192a77efedec80d
Steven-mejia726 <st559049@ucf.edu>
2024-09-04 18:41:33 +0000

    Finished calculateArea()

--- fracture.c
+++ fracture.c
@@ -2,15 +2,8 @@
 #include <stdio.h>
 #include <math.h>
 
-double userInputs();
-double calculateDistance();
-//Creating main function
-int main(int argc, char **argv) 
-{
-  double test = calculateDistance();  
-
-  return 0;
-}
+//Define Pi
+#define PI 3.14159
 
 //Creating a function to ask for user input.
 double userInputs() 
@@ -37,18 +30,21 @@ double calculateDistance()
   //Asking user for inputs on position of points
   printf("Type in x coordinate of first point: ");
   x1 = userInputs();
-  printf("Type in y coordinate of first point: ");
-  y1 = userInputs();
-  printf("Type in x coordinate of first point: ");
+  printf("Type in x coordinate of second point: ");
   x2 = userInputs();
   printf("Type in y coordinate of first point: ");
+  y1 = userInputs();
+  printf("Type in y coordinate of second point: ");
   y2 = userInputs();
   
   //Printing out user inputs
   printf("Point #1 entered: x1 = %.3lf; y1 = %.3lf \n", x1, y1);
   printf("Point #2 entered: x2 = %.3lf; y2 = %.3lf \n", x1, y1);
 
+  //Calculating distance between two points
   totalDistance = sqrt(pow(x2+x1, 2) + pow(y2+y1, 2));
+
+  //Priting distance between two points
   printf("The distance between the two points is %.3f \n", totalDistance);
   return totalDistance;
 }
@@ -56,7 +52,17 @@ double calculateDistance()
 //Creating function to calculate perimeter of a city 
 double calculatePerimeter() 
 {
+  //Initialized variables
+  double perimeter =0.0;
+  double diameter = calculateDistance();
 
+  //Making equation to find perimeter
+  perimeter = PI * diameter;
+
+  //Printing out perimeter
+  printf("The perimeter of the city encompassed by your request is %.3lf \n", perimeter);
+
+return 3.0;
 }
 
 //Creating function that calculates the area of a city encompessed by two points
@@ -77,3 +83,11 @@ double caluclateHeight()
 
 }
 
+//Creating main function
+int main(int argc, char **argv) 
+{
+  double distance = calculateDistance();
+  double test = calculatePerimeter();  
+
+  return 0;
+}
