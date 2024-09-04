12205f8f7ca435d618699e85bcb66339bb7e152c
Steven-mejia726 <st559049@ucf.edu>
2024-09-04 17:18:13 +0000

    totalDistance() complete

--- fracture.c
+++ fracture.c
@@ -2,36 +2,54 @@
 #include <stdio.h>
 #include <math.h>
 
+double userInputs();
+double calculateDistance();
 //Creating main function
 int main(int argc, char **argv) 
 {
-  
+  double test = calculateDistance();  
 
   return 0;
 }
+
+//Creating a function to ask for user input.
+double userInputs() 
+{ 
+  //Initializing variable
+  double userInput = 0.0;
+    
+  //Asking for user input
+  scanf("%lf", &userInput);
+  return userInput;
+}
+
 
 //Creating function to calculate distance between two points
 double calculateDistance()
 {
   //Initializing variable
-  double x1 = 0.0
-  double x2 = 0.0
-  double y1 = 0.0
-  double y2 = 0.0
+  double x1 = 0.0;
+  double x2 = 0.0;
+  double y1 = 0.0;
+  double y2 = 0.0;
   double totalDistance = 0.0;
 
-  //Printing out user inputed info
-  printf("Point 1 entered: x1 = ");
+  //Asking user for inputs on position of points
+  printf("Type in x coordinate of first point: ");
   x1 = userInputs();
-  printf("; y1 = ");
+  printf("Type in y coordinate of first point: ");
   y1 = userInputs();
-  printf("Point 2 was entered: x2 = ");
+  printf("Type in x coordinate of first point: ");
   x2 = userInputs();
-  printf("; y2 = ");
+  printf("Type in y coordinate of first point: ");
   y2 = userInputs();
+  
+  //Printing out user inputs
+  printf("Point #1 entered: x1 = %.3lf; y1 = %.3lf \n", x1, y1);
+  printf("Point #2 entered: x2 = %.3lf; y2 = %.3lf \n", x1, y1);
 
   totalDistance = sqrt(pow(x2+x1, 2) + pow(y2+y1, 2));
-  printf(totalDistance);
+  printf("The distance between the two points is %.3f \n", totalDistance);
   return totalDistance;
 }
 
@@ -59,13 +77,3 @@ double caluclateHeight()
 
 }
 
-//Creating a function to ask for user input.
-double userInputs() 
-{
-  //Initializing variable
-    double userInput = 0.0
-    
-    //Asking for user input
-    scanf(%lf, &userInput);
-    return userInput;
-}
